24c0dd7bf6c5aa8ea45c56dd82c679d7fe720cee
Prithviraj Awatade <96919987+prithvi009@users.noreply.github.com>
2024-04-20 21:36:36 +0530

    feat: schema defined

---
+++ backend/Model/LiveStream.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+const LiveStreamSchema = new mongoose.Schema({
+    id: {
+        type: Number,
+        require: true,
+    },
+    name: {
+        type: String,
+        require: true,
+    },
+    image: {
+        type: String,
+        require: true,
+    },
+    category: {
+        type: String,
+        require: true,
+    },
+    new_price: {
+        type: Number,
+        required: true,
+    },
+    old_price: {
+        type: Number,
+        required: true,
+    },
+    url: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+    availabe: {
+        type: Boolean,
+        defaut: true,
+    }
+})
+
+module.exports = mongoose.model('LiveStream', LiveStreamSchema)
