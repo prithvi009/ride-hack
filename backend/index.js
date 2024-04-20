eb1606258564a6b2d42b786fe34fb869dca07f70
Prithviraj Awatade <96919987+prithvi009@users.noreply.github.com>
2024-04-20 18:18:04 +0530

    feat: server setup

---
+++ backend/index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express();
+
+
+require('dotenv').config();
+
+const port = process.env.PORT || 5000
+
+app.use(express.json())
+app.use(cors())
+
+
+//IMAGE STORAGE ENGINE
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './Upload/images')
+    },
+    filename: function (req, file, cb) {
+        cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    }
+})
+
+const upload = multer({ storage: storage })
+
+app.use('/images', express.static('./Upload/images'))
+
+//if no url satisfied
+app.use(notfound)
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, () => {
+            console.log(`server is listening at ${port}`)
+        })
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
+
+start()
